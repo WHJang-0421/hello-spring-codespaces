95fd470d0bd73118938a94dd45e6c2d52460cfab
WHJang-0421 <morrranii@gmail.com>
2024-11-26 18:36:31 +0900

    Add files via upload

---
+++ hello-codespaces/src/main/java/com/example/hello_codespaces/HelloCodespacesApplication.java
@@ -0,0 +1,13 @@
+package com.example.hello_codespaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloCodespacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloCodespacesApplication.class, args);
+	}
+
+}
---
+++ hello-codespaces/src/test/java/com/example/hello_codespaces/HelloCodespacesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hello_codespaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloCodespacesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
