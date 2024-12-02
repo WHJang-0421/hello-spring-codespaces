3a9c8723318bfe5265d3e641dd97cff32e7145fb
WHJang-0421 <morrranii@gmail.com>
2024-12-02 09:42:20 +0000

    Connect Spring to Postgres

--- hello-codespaces/src/main/java/com/example/hello_codespaces/service/TaskService.java
+++ hello-codespaces/src/main/java/com/example/hello_codespaces/service/TaskService.java
@@ -4,7 +4,6 @@ import com.example.hello_codespaces.domain.Task;
 import com.example.hello_codespaces.dto.TaskDto;
 import com.example.hello_codespaces.repository.TaskRepository;
 
-import org.hibernate.cache.spi.support.AbstractReadWriteAccess.Item;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
