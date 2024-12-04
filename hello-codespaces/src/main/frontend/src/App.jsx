b9696e9a0d31dcd81b38d5f61d1ce2e5a916354f
WHJang-0421 <morrranii@gmail.com>
2024-12-04 14:18:17 +0000

    Use Vite

--- hello-codespaces/src/main/frontend/src/App.jsx
+++ hello-codespaces/src/main/frontend/src/App.jsx
@@ -1,22 +1,71 @@
-// import logo from './logo.svg';
 import './App.css';
 
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
-  const Item = ({ text, completed, id }) => {
+export default function App() {
+  const Item = ({
+    text,
+    completed,
+    id,
+    updateCompleted,
+    deleteTodo,
+    updateText
+  }) => {
+    const [edit, setEdit] = useState(false);
+    const [editText, setEditText] = useState(text);
+
     return (
       <div className="item">
-        <div class="circle">{completed ? <span>&#10003;</span> : ""}</div>
-        <div className={completed ? "strike" : ""}>{text}</div>
-        <div class="close">X</div>
+        <div class="circle" onClick={() => updateCompleted(id)}>
+          {completed ? <span>&#10003;</span> : ""}
+        </div>
+        <div
+          className={completed ? "strike" : ""}
+          onDoubleClick={() => {
+            if (!completed) {
+              setEdit(true);
+            }
+          }}
+        >
+          {edit ? (
+            <input
+              type="text"
+              value={editText}
+              onChange={(e) => {
+                setEditText(e.target.value);
+              }}
+              onBlur={() => {
+                setEdit(false);
+                updateText(id, editText);
+              }}
+            />
+          ) : (text)}
+        </div>
+        <div class="close" onClick={() => deleteTodo(id)}>
+          X
+        </div>
       </div>
     );
   };
 
   const [todos, setTodos] = useState([]);
   const inputRef = useRef();
-  function handleKeyPress(e) {
+
+  // useEffect(() => {
+  //   fetchTodos()
+  // }, []);
+  // function fetchTodos() {
+  //   axios.get('/api/v1/tasks')
+  //     .then((response) => {
+  //       setTodos(response.data);
+  //     })
+  //     .catch((error) => {
+  //       console.log("Error while fetching tasks:", error);
+  //     })
+  // }
+
+  const handleKeyPress = (e) => {
     if (e.key === "Enter") {
       setTodos([
         ...todos,
@@ -24,16 +73,52 @@ function App() {
       ]);
       inputRef.current.value = "";
     }
-  }
+  };
 
+  // toggle completed
+  const handleCompleted = (id) => {
+    const updatedList = todos.map((e) => {
+      if (e.id === id) {
+        e.completed = !e.completed;
+      }
+
+      return e;
+    });
+
+    setTodos(updatedList);
+  };
+
+  // delete item
+  const handleDelete = (id) => {
+    const filter = todos.filter((e) => e.id !== id);
+    setTodos(filter);
+  };
+
+  // handle text update
+  const handleUpdateText = (id, text) => {
+    const updatedList = todos.map((e) => {
+      if (e.id === id) {
+        e.text = text;
+      }
+
+      return e;
+    });
+
+    setTodos(updatedList);
+  };
 
   return (
     <div className="App">
       <input type="text" onKeyUp={handleKeyPress} ref={inputRef} />
-      {todos.map(e =>
-        <Item {...e} key={e.id} />
+      {todos.map((e) =>
+      (<Item
+        {...e}
+        key={e.id}
+        updateCompleted={handleCompleted}
+        deleteTodo={handleDelete}
+        updateText={handleUpdateText}
+      />)
       )}
     </div>
   );
 }
-export default App;
